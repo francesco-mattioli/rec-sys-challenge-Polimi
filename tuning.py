85ae5038302f6439049a1e433353ebf65bfda277
francesco-mattioli <francesco1.mattioli@mail.polimi.it>
2022-12-07 21:06:02 +0100

    added notebook for hybrid

--- tuning.py
+++ tuning.py
@@ -8,7 +8,7 @@ from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommende
 from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
 from Recommenders.GraphBased.P3alphaRecommender import P3alphaRecommender
-
+from Recommenders.FactorizationMachines.LightFMRecommender import LightFMItemHybridRecommender
 from Evaluation.Evaluator import EvaluatorHoldout
 from Recommenders.DataIO import DataIO
 from HyperparameterTuning.SearchBayesianSkopt import SearchBayesianSkopt
@@ -21,18 +21,18 @@ dataReader = DataReader()
 #urm = dataReader.load_binary_urm()
 urm = dataReader.load_augmented_binary_urm()
 #urm = dataReader.load_powerful_binary_urm()
+urm, icm = dataReader.paddingICMandURM(urm)
 target = dataReader.load_target()
 # dataReader.print_statistics(target)
 
 #URM_train_v0, URM_test = split_train_in_two_percentage_global_sample(urm, train_percentage=0.90)
 #URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train_v0, train_percentage=0.90)
 URM_train, URM_validation = split_train_in_two_percentage_global_sample(urm, train_percentage=0.9)
-
-
+#URM_train, icm = dataReader.paddingICMandURM(urm)
 evaluator_validation = EvaluatorHoldout(URM_validation, [10])
 #evaluator_test = EvaluatorHoldout(URM_test, [10])
 
-recommender_class = P3alphaRecommender
+recommender_class = LightFMItemHybridRecommender
 
 output_folder_path = "result_experiments/"
 
@@ -45,32 +45,44 @@ n_random_starts = int(n_cases*0.3)
 metric_to_optimize = "MAP"   
 cutoff_to_optimize = 10
 
+earlystopping_keywargs = {"validation_every_n": 5,
+                              "stop_on_validation": True,
+                              "evaluator_object": evaluator_validation,
+                              "lower_validations_allowed": 5,
+                              "validation_metric": metric_to_optimize,
+                              }
+
 hyperparameters_range_dictionary = {
-                "topK": Integer(5, 1000),
-                "alpha": Real(low = 0, high = 2, prior = 'uniform'),
-                "normalize_similarity": Categorical([True, False]),
-            }
+                    "epochs": Categorical([300]),
+                    "n_components": Integer(1, 200),
+                    "loss": Categorical(['bpr', 'warp', 'warp-kos']),
+                    "sgd_mode": Categorical(['adagrad', 'adadelta']),
+                    "learning_rate": Real(low = 1e-6, high = 1e-1, prior = 'log-uniform'),
+                    "item_alpha": Real(low = 1e-5, high = 1e-2, prior = 'log-uniform'),
+                    "user_alpha": Real(low = 1e-5, high = 1e-2, prior = 'log-uniform'),
+                }
+
+recommender_input_args = SearchInputRecommenderArgs(
+    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, icm],
+    CONSTRUCTOR_KEYWORD_ARGS = {},
+    FIT_POSITIONAL_ARGS = [],
+    FIT_KEYWORD_ARGS = {},
+    EARLYSTOPPING_KEYWORD_ARGS = earlystopping_keywargs,
+)
+
 
 
 #create a bayesian optimizer object, we pass the recommender and the evaluator
 hyperparameterSearch = SearchBayesianSkopt(recommender_class,
                                          evaluator_validation=evaluator_validation)
 
-#provide data needed to create instance of model (one on URM_train, the other on URM_all)
-recommender_input_args = SearchInputRecommenderArgs(
-    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train],     # For a CBF model simply put [URM_train, ICM_train]
-    CONSTRUCTOR_KEYWORD_ARGS = {},
-    FIT_POSITIONAL_ARGS = [],
-    FIT_KEYWORD_ARGS = {},
-    EARLYSTOPPING_KEYWORD_ARGS = {},
-)
 
 recommender_input_args_last_test = SearchInputRecommenderArgs(
     CONSTRUCTOR_POSITIONAL_ARGS = [urm],
     CONSTRUCTOR_KEYWORD_ARGS = {},
     FIT_POSITIONAL_ARGS = [],
     FIT_KEYWORD_ARGS = {},
-    EARLYSTOPPING_KEYWORD_ARGS = {},
+    EARLYSTOPPING_KEYWORD_ARGS = earlystopping_keywargs,
 )
 
 
